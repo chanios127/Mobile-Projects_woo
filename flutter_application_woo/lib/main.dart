cb9df07bc0512870cf4a8a4ed00be1bd90c51ccd
chanios127 <chanios127@gmail.com>
2024-12-19 09:55:10 +0900

    mac setup

--- flutter_application_woo/lib/main.dart
+++ flutter_application_woo/lib/main.dart
@@ -3,123 +3,232 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+/*
+class MyApp extends StatelessWidget {
+ const MyApp({super.key});
+
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: Scaffold(
+       appBar: AppBar(
+         centerTitle: true,
+         title: const Text(
+           "Hello Flutter",
+           style: TextStyle(fontSize: 28),
+         ),
+       ),
+
+       drawer: Drawer(
+         child: ListView(
+           padding: EdgeInsets.zero,
+           children: [
+             DrawerHeader(
+               decoration: BoxDecoration(
+                 color: Colors.blue,
+               ),
+               child: Column(
+                 children: [
+                   Text(
+                     '메뉴',
+                     style: TextStyle(
+                       color: Colors.white,
+                       fontSize: 24,
+                     ),
+                   ),
+                   Padding(
+                     padding: const EdgeInsets.all(32),
+                     child: Container(,
+                       child: Image.network(
+                         "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                         width: 23,
+                       ),
+                     ),
+                   ),
+                 ],
+               ),
+             ),
+           ],
+         ),
+       ),
+
+       body: SingleChildScrollView(
+         child: Padding(
+           padding: const EdgeInsets.all(16),
+           child: Column(
+             children: [
+               Padding(
+                 padding: const EdgeInsets.all(32),
+                 child: Image.network(
+                   "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                   width: 81,
+                 ),
+               ),
+               const TextField(
+                 decoration: InputDecoration(
+                   labelText: "이메일",
+                 ),
+               ),
+               const TextField(
+                 obscureText: true,
+                 decoration: InputDecoration(
+                   labelText: "비밀번호",
+                 ),
+               ),
+               Container(
+                 width: double.infinity,
+                 margin: const EdgeInsets.only(top: 24),
+                 child: ElevatedButton(
+                   onPressed: () {},
+                   child: const Text("로그인"),
+                 ),
+               )
+             ],
+           ),
+         ),
+       ),
+     ),
+   );
+ }
+}*/
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        // mac test
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(), // 홈페이지 보여주기
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // 음식 사진 데이터
+    List<Map<String, dynamic>> dataList = [
+      {
+        "category": "수제버거",
+        "imgUrl":
+            "https://i.ibb.co/HBGKYn4/foodiesfeed-com-summer-juicy-beef-burger.jpg",
+      },
+      {
+        "category": "건강식",
+        "imgUrl":
+            "https://i.ibb.co/mB5YNs2/foodiesfeed-com-pumpkin-soup-with-pumpkin-seeds-on-top.jpg",
+      },
+      {
+        "category": "한식",
+        "imgUrl":
+            "https://i.ibb.co/Kzzpc97/Beautiful-vibrant-shot-of-traiditonal-Korean-meals.jpg",
+      },
+      {
+        "category": "디저트",
+        "imgUrl":
+            "https://i.ibb.co/DL5vJVZ/foodiesfeed-com-carefully-putting-a-blackberry-on-tiramisu.jpg",
+      },
+      {
+        "category": "피자",
+        "imgUrl": "https://i.ibb.co/qsm8QH4/pizza.jpg",
+      },
+      {
+        "category": "볶음밥",
+        "imgUrl":
+            "https://i.ibb.co/yQDkq2X/foodiesfeed-com-hot-shakshuka-in-a-cast-iron-pan.jpg",
+      },
+    ];
+
+    // 화면에 보이는 영역
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      //Drawer==================================================================================================================================================================
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    '메뉴',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(32),
+                    child: Image.network(
+                      "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                      width: 23,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //Drawer==================================================================================================================================================================
+
+      //appBar==================================================================================================================================================================
+      appBar: AppBar(
+        elevation: 0, // 그림자 없애기
+        backgroundColor: Colors.white, // 배경 색상
+        centerTitle: false, // title 중앙 정렬
+        iconTheme: IconThemeData(color: Colors.black), // app bar icon color
+        title: Text(
+          "Food Recipe",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          /// My Button
+          IconButton(
+            onPressed: () {
+              print("go my page");
+            },
+            icon: Icon(
+              Icons.person_outline,
+            ),
+          ),
+        ],
+      ),
+      //appBar==================================================================================================================================================================
+
+      //Body==================================================================================================================================================================
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Search Bar====================================================================================
+            Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.search),
+                    hintText: "상품을 검색해주세요.",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                  ),
+                ),
+                margin: EdgeInsets.all(8)),
+
+            //Categories====================================================================================
+            //Expanded(child:,)
+          ],
+        ),
+      ),
+      //Body==================================================================================================================================================================
     );
   }
 }
