baf1b2d16a21eed881c6b2001001a38c9ead28d4
chanios127 <chanios127@gmail.com>
2024-12-19 16:32:53 +0900

    food receipe sample task

--- flutter_application_woo/lib/main.dart
+++ flutter_application_woo/lib/main.dart
@@ -3,95 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-/*
-class MyApp extends StatelessWidget {
- const MyApp({super.key});
-
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: Scaffold(
-       appBar: AppBar(
-         centerTitle: true,
-         title: const Text(
-           "Hello Flutter",
-           style: TextStyle(fontSize: 28),
-         ),
-       ),
-
-       drawer: Drawer(
-         child: ListView(
-           padding: EdgeInsets.zero,
-           children: [
-             DrawerHeader(
-               decoration: BoxDecoration(
-                 color: Colors.blue,
-               ),
-               child: Column(
-                 children: [
-                   Text(
-                     '메뉴',
-                     style: TextStyle(
-                       color: Colors.white,
-                       fontSize: 24,
-                     ),
-                   ),
-                   Padding(
-                     padding: const EdgeInsets.all(32),
-                     child: Container(,
-                       child: Image.network(
-                         "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
-                         width: 23,
-                       ),
-                     ),
-                   ),
-                 ],
-               ),
-             ),
-           ],
-         ),
-       ),
-
-       body: SingleChildScrollView(
-         child: Padding(
-           padding: const EdgeInsets.all(16),
-           child: Column(
-             children: [
-               Padding(
-                 padding: const EdgeInsets.all(32),
-                 child: Image.network(
-                   "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
-                   width: 81,
-                 ),
-               ),
-               const TextField(
-                 decoration: InputDecoration(
-                   labelText: "이메일",
-                 ),
-               ),
-               const TextField(
-                 obscureText: true,
-                 decoration: InputDecoration(
-                   labelText: "비밀번호",
-                 ),
-               ),
-               Container(
-                 width: double.infinity,
-                 margin: const EdgeInsets.only(top: 24),
-                 child: ElevatedButton(
-                   onPressed: () {},
-                   child: const Text("로그인"),
-                 ),
-               )
-             ],
-           ),
-         ),
-       ),
-     ),
-   );
- }
-}*/
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -207,7 +118,7 @@ class HomePage extends StatelessWidget {
       //appBar==================================================================================================================================================================
 
       //Body==================================================================================================================================================================
-      body: SingleChildScrollView(
+      body: Container(
         child: Column(
           children: [
             //Search Bar====================================================================================
@@ -224,7 +135,34 @@ class HomePage extends StatelessWidget {
                 margin: EdgeInsets.all(8)),
 
             //Categories====================================================================================
-            //Expanded(child:,)
+            Expanded(
+                child: ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 4.0,
+                  child: Container(
+                    child: Stack(children: [
+                      Image.network(
+                        dataList[index]["imgUrl"],
+                        fit: BoxFit.fitHeight,
+                      ),
+                      Center(
+                        child: Text(
+                          dataList[index]["category"],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ]),
+                    height: 100,
+                  ),
+                );
+              },
+            ))
           ],
         ),
       ),
